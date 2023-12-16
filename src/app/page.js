bf39ca8964208f53a5cdef7abd75864d8782e46c
SamYusuf101 <samyusuf101@gomail.com>
2023-12-16 11:41:34 +0100

    created form

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,46 @@
 "use client";
-export default function Home() { 
+export default function Home() {
   return (
     <div className="pl-10 mt-10">
-    <h1 className="mb-5 text-3xl">Contact Management App</h1>
+      <h1 className="mb-5 text-3xl">Contact Management App</h1>
+      <form>
+        <label className="block text-sm font-medium text-gray-700 ">
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label className="block text-sm font-medium text-gray-700">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label className="block text-sm font-medium text-gray-700">
+          Phone:
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+        <br />
+        <button
+          type="button"
+          onClick={addContact}
+          className="py-2 px-4 bg-red-500 text-white font-semibold mt-3
+            rounded-lg shadow-md hover:bg-yellow-400 
+            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          Add Contact
+        </button>
+      </form>
     </div>
-  )
-
+  );
 }
